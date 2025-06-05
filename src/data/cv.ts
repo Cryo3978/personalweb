e31548226299778fe60281d0f9ab6bf0090b10da
Cryo3978 <chenanran0711@outlook.com>
2025-06-05 04:07:10 -0700

    1

---
+++ src/data/cv.ts
@@ -0,0 +1,126 @@
+export const experiences = [
+	{
+		company: 'China Fortune Securities Co., Ltd',
+		time: 'May 2023 - Aug 2023',
+		title: 'Data Analyst Intern',
+		location: 'Shanghai, China',
+		// description: 'Python, SQL and Data analysis. Process and analyze the financial data of the bond industry, and cooperate',
+	},
+	{
+		company: 'Autonomous Driving Laboratory of Tsinghua University',
+		time: 'Mar 2023 - July 2023',
+		title: 'Research Assistant',
+		location: 'Peiking, China',
+		// description: 'Led groundbreaking studies on radioactivity and mentored future Nobel Prize laureates.',
+	},
+	{
+		company: 'University of Southern California',
+		time: 'Jan 2025 - Present',
+		title: 'Teaching Assistant',
+		location: 'Los Angeles, CA, US',
+		// description: 'Led groundbreaking studies on radioactivity and mentored future Nobel Prize laureates.',
+	},
+];
+
+export const education = [
+	{
+		school: 'University of Southern California',
+		time: '2024 - 2026',
+		degree: 'MS. in Data Analytics',
+		location: 'Los Angeles, California, United States',
+		description: '',
+	},
+	{
+		school: 'Tianjin University',
+		time: '2020 - 2024',
+		degree: 'BE. in Industrial Design',
+		location: 'Tianjin, China',
+		description: '',
+	},
+];
+
+export const skills = [
+	{
+		title: 'C++, C; Python; SQL; html, css, JavaScript; Data analysis; LLM; Computer Vision; AI + Medicine or Fintech',
+		description: '',
+	},
+	// {
+	// 	title: 'Experimental Techniques',
+	// 	description: 'Spectroscopy, Isolation of Radioactive Elements, Radiation Measurement',
+	// },
+];
+
+export const publications = [
+	{
+		title: 'Predicting ICU Readmission in Acute Pancreatitis Patients Using a Machine Learning-Based Model with Enhanced Clinical Interpretability',
+		authors: 'Shuheng Chen, Yong Si, Junyi Fan, Li Sun, Elham Pishgar, Kamiar Alaei, Greg Placencia, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'May 11, 2025',
+		link: 'https://arxiv.org/abs/2505.14850',
+		abstract: '',
+	},
+	{
+		title: 'Machine Learning-Based Prediction of Mortality in Geriatric Traumatic Brain Injury Patients',
+		authors: 'Yong Si, Junyi Fan, Li Sun, Shuheng Chen, Elham Pishgar, Kamiar Alaei, Greg Placencia, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'May 19, 2025',
+		link: 'https://arxiv.org/abs/2505.15850',
+		abstract: '',
+	},
+	{
+		title: 'Machine Learning-Based Prediction of Mortality in Geriatric Traumatic Brain Injury PatientsMachine Learning-Based Model for Postoperative Stroke Prediction in Coronary Artery Disease',
+		authors: 'Haonan Pan, Shuheng Chen, Elham Pishgar, Kamiar Alaei, Greg Placencia, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'March 15, 2025',
+		link: 'https://arxiv.org/abs/2503.11973',
+		abstract: '',
+	},
+	{
+		title: 'Retrospective machine learning approach for forecasting in-hospital death in icu patients after cardiac arrest',
+		authors: 'Yong Si, Li Sun, Shuheng Chen, JunYi Fan, Elham Pishgar, Kamiar Alaei, Greg Placencia, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'May 5, 2025',
+		link: 'https://www.medrxiv.org/content/10.1101/2025.05.05.25327009v1',
+		abstract: '',
+	},
+	{
+		title: 'Predicting 28-Day Mortality in First-Time ICU Patients with Heart Failure and Hypertension Using LightGBM: A MIMIC-IV Study',
+		authors: 'Junyi Fan, Shuheng Chen, Haonan Pan, Elham Pishgar, Greg Placencia, Kamiar Alaei, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'March 28, 2025',
+		link: 'https://www.medrxiv.org/content/10.1101/2025.03.28.25324829v1',
+		abstract: '',
+	},
+	{
+		title: 'Utilizing Machine Learning Models to Predict Acute Kidney Injury in Septic Patients from MIMIC-III Database',
+		authors: 'Aleyeh Roknaldin, Zehao Zhang, Jiayuan Xu, Kamiar Alaei, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'Dec 4, 2024',
+		link: 'https://arxiv.org/abs/2412.03737',
+		abstract: '',
+	},
+	{
+		title: 'Development of Interactive Nomograms for Predicting Short-Term Survival in ICU Patients with Aplastic Anemia',
+		authors: 'Junyi Fan, Shuheng Chen, Li Sun, Yong Si, Elham Pishgar, Kamiar Alaei, Greg Placencia, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'May 23, 2025',
+		link: 'https://arxiv.org/abs/2505.18421',
+		abstract: '',
+	},
+	{
+		title: 'Machine Learning-Based Prediction of ICU Mortality in Sepsis-Associated Acute Kidney Injury Patients Using MIMIC-IV Database with Validation from eICU Database',
+		authors: 'Shuheng Chen, Junyi Fan, Elham Pishgar, Kamiar Alaei, Greg Placencia, Maryam Pishgar',
+		journal: 'arXiv preprint',
+		time: 'Feb 25, 2025',
+		link: 'https://arxiv.org/abs/2502.17978',
+		abstract: '',
+	},
+	{
+		title: 'Machine Learning-Based Prediction of ICU Readmissions in Intracerebral Hemorrhage Patients: Insights from the MIMIC Databases',
+		authors: 'Shuheng Chen, Junyi Fan, Armin Abdollahi, Negin Ashrafi, Kamiar Alaei, Greg Placencia, Maryam Pishgar',
+		journal: '13th SAI International Computing Conference',
+		time: 'Jan 1, 2025',
+		link: 'https://arxiv.org/abs/2501.01183',
+		abstract: '',
+	},
+];
